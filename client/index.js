a6cbaa388ef8ede079440c7fb4ff34c026f0a3b7
Ali Shahabbasi <shahabbasi.ali.98@gmail.com>
2019-11-19 16:43:03 +0330

    Init

---
+++ client/index.js
@@ -0,0 +1,3 @@
+import './scripts/index.js'
+
+
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './client/index.js',
+    output: {
+        filename: './javascripts/bundle.js',
+        sourceMapFilename: './javascripts/bundle.js.map'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015'] }
+            }]
+    }
+};
